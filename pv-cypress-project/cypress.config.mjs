72cd2fac728c1e4d2b3676748ef332d3b7b66c3a
Stacy Balyaeva <balyaevanast@email.cz>
2025-06-05 19:20:58 +0200

    2.commit

--- pv-cypress-project/cypress.config.mjs
+++ pv-cypress-project/cypress.config.mjs
@@ -10,8 +10,10 @@ function getConfigurationByFile(file) {
 }
 
 export default defineConfig({
+  defaultCommandTimeout: 30000,
   e2e: {
     watchForFileChanges: false,
+    projectId: "eiu8er",
     setupNodeEvents(on, config) {
       const file = config.env.configFile || 'staging';
       const customEnv = getConfigurationByFile(file);
